d2336993ce2c512ef1cb68319176a3b91a4d4228
Station2 <station2@Station2.local>
2016-10-14 12:03:16 -0400

    Added Footer Links and Styled

---
+++ builds/development/wp-content/themes/papi-wholesale/footer.php
@@ -0,0 +1,62 @@
+        <div class="footer_wrap ">
+
+
+                <footer id="footer" class="container">
+                    <div class="sub_footer">
+                            
+                            <div class="col-md-3">
+                                <ul>
+                                    <h3>Real Life Papi</h3>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Silhouettes</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-3">
+                                <ul>
+                                    <h3>Account</h3>
+                                    <li><a href="#">My Account</a></li>
+                                    <li><a href="#">My Cart</a></li>
+                                    <li><a href="#">Checkout</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-3">
+                                <ul>
+                                    <h3>Shop Papi</h3>
+                                    <li><a href="#">All Styles</a></li>
+                                    <li><a href="#">Collections</a></li>
+                                    <li><a href="#">Multipacks</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-3">
+                                <ul>
+                                    <h3>Ask Papi</h3>
+                                    <li><a href="#">Fabrications</a></li>
+                                    <li><a href="#">Shipping & Returns</a></li>
+                                    <li><a href="#">Terms & Conditions</a></li>
+                                </ul>
+                            </div>
+                           
+                        <div class="clearfix"></div>
+                    </div><!-- /sub_footer -->
+                </footer><!-- /footer -->
+
+
+            <div class="clearfix"></div>
+                
+        </div><!-- /footer_wrap -->
+
+        </div><!-- /wrap -->
+
+    
+        
+    <!-- WP_Footer --> 
+    <?php wp_footer(); ?>
+    <!-- /WP_Footer --> 
+
+      
+    </body>
+</html>
